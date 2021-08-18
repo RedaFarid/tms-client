38e51fbcdfef76ab079eba15b7719cd6f82cdc13
Reda farid <reda.farid.abdelhamid@gmail.com>
2021-08-18 14:40:54 +0200

    v1.0

---
+++ src/main/java/soulintec/com/tmsclient/TmsClientApplication.java
@@ -0,0 +1,13 @@
+package soulintec.com.tmsclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmsClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmsClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/soulintec/com/tmsclient/TmsClientApplicationTests.java
@@ -0,0 +1,13 @@
+package soulintec.com.tmsclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmsClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
